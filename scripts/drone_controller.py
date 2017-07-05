24ce668722d67094f7940d998cc5d5715976d48d
Flying85 <Flying85.User>
2017-07-05 10:27:48 +0100

    Upgraded all control loops to PID using brl_drones/Rospid with external tuning GUIs in launch.  To be tuned.

--- scripts/drone_controller.py
+++ scripts/drone_controller.py
@@ -3,17 +3,24 @@ import roslib
 roslib.load_manifest('optical_ardrone')
 import rospy
 from geometry_msgs.msg import Polygon, Twist
+from brl_drones.rospid import Rospid
 
 class DroneController:
 
   def __init__(self):
     rospy.init_node('drone_controller', anonymous=True)
     self.twist_pub = rospy.Publisher("ext_vel",Twist,queue_size=1)
+    self.height_pid = Rospid(2.0, 0.0, 0.0, 'control/height', lower_lim=-0.3, upper_lim=0.3)
+    self.lateral_pid = Rospid(2.0, 0.0, 0.0, 'control/roll', lower_lim=-0.3, upper_lim=0.3)
+    self.longitudinal_pid = Rospid(2.0, 0.0, 0.0, 'control/pitch', lower_lim=-0.3, upper_lim=0.3)
+    self.yaw_pid = Rospid(1.0, 0.0, 0.0, 'control/yaw', lower_lim=-0.3, upper_lim=0.3)
 
   def start(self):
     self.points_poly_sub = rospy.Subscriber("points_out",Polygon,self.callback)
 
   def callback(self,data):
+    # need the time
+    time_now = rospy.get_time()
     # default message
     t = Twist()
     # check number of blobs provided
@@ -22,18 +29,18 @@ class DroneController:
       # calculated weighted centroid in x
       size_sum = sum([p.z for p in data.points])
       centroid_x = sum([p.x*p.z for p in data.points])/size_sum    
-      # track centroid with yaw
-      t.angular.z = 1.0*(0.5 - centroid_x)
+      # track centroid with yaw      
+      t.angular.z = self.yaw_pid.update(centroid_x, 0.5, time_now)
     # the rest only works if exactly three points found
     if num_points<>3:
       rospy.logwarn("Got %d points - was expecting 3" % num_points)
     else:
       # lateral control to line up middle point with outer two
-      t.linear.y = -2.0*(data.points[1].x - 0.5*data.points[0].x - 0.5*data.points[2].x)
+      t.linear.y = self.lateral_pid.update(data.points[1].x - 0.5*data.points[0].x - 0.5*data.points[2].x, 0.0, time_now)
       # height control to line up middle point with outer two
-      t.linear.z = -2.0*(data.points[1].y - 0.5*data.points[0].y - 0.5*data.points[2].y)
+      t.linear.z = self.height_pid.update(data.points[1].y - 0.5*data.points[0].y - 0.5*data.points[2].y, 0.0, time_now)
       # longitudinal control to keep the back two fixed angle apart
-      t.linear.x = -1.0*(data.points[2].x - data.points[0].x - 0.2)
+      t.linear.x = self.longitudinal_pid.update(data.points[2].x - data.points[0].x, 0.2, time_now)
     # send command anyway
     self.twist_pub.publish(t)
 
